6f3ce4af9d4db6e2d293b674461711a80bc6dfa1
n0rdp0l <63914035+n0rdp0l@users.noreply.github.com>
2022-04-04 20:48:42 +0200

    Update gradedb.py

--- gradedb.py
+++ gradedb.py
@@ -1,1 +1,7 @@
 # python file with definitions of the main project class GradeDB with access methods to the database. (.py)
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import relationship
